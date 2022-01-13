9bdb38198c6dc40f15d78676f2c01c2ced42e523
francescofact <ffattori@me.com>
2022-01-13 14:24:18 +0100

    Initial Commit

---
+++ DualDimmer/DualDimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualDimmerApp.swift
+//  DualDimmer
+//
+//  Created by Francesco Fattori on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DualDimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
